293c3c6db304f9eb6c658b95d9fb20c6a8ff7738
HVMien <MienHV1@fsoft.com.vn>
2017-04-28 16:03:37 +0700

    Revert "trêtr"
    This reverts commit 4d12e80b308ff916d60f91bb4b61f03ae50da6dd.

--- app/src/main/java/com/example/mienhv1/survey/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/mienhv1/survey/ui/home/HomeActivity.java
@@ -47,8 +47,6 @@ public class HomeActivity extends BaseActivity implements HomeFragment.OnHomeLis
                 "Yes",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        HomeActivity.super.onBackPressed();
-                        finish();
                     }
                 });
 
